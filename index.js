bff69360669eaa9ee3c605766d92ab4e913031fb
Gabriel Torres <e107ti@etec.sp.gov.br>
2023-04-06 22:44:30 -0300

    Adiciona solução do exercício nome completo js

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = prompt ("Digite seu primeiro nome");
+let sobrenome = prompt ("Digite seu Sobrenome");
+let nomeCompleto = `${nome} ${sobrenome}`;
+let nomeCatalogo = `${sobrenome.toUpperCase()} ${nome}`;
+
+alert (nomeCompleto);
+alert (nomeCatalogo);
